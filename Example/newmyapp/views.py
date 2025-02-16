bb442f17a8966bd34c17afe2870b2192b79e1afe
24-python <24_python@mail.ru>
2025-02-16 12:45:35 +0300

    Итоговая домашка

--- Example/newmyapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def data(request):
-    return HttpResponse("<h1>Страница с данными проекта Django</h1>")
-
-def test(request):
-    return HttpResponse("<h1>Это тестовая страница на Django</h1>")
