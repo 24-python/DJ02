fc7891cd8526ac2fbcaa3e666075c4f09889c6b3
24-python <24_python@mail.ru>
2025-02-16 12:11:47 +0300

    1

--- Example/main/views.py
+++ Example/main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', context={'title': 'Первая страница на Django'})
 
 def new(request):
-    return render(request, 'main/new.html')
+    return render(request, 'main/new.html', context={'title': 'Вторая страница на Django', 'name': 'ДжанГО'})
+
+def nav(request):
+    return render(request, 'main/nav.html')
